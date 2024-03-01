fde8e44f713939760c816995d0687a8463bbaa75
Efial <84471368+Efial@users.noreply.github.com>
2024-03-01 07:04:18 -0500

    Update InfTime.bat

--- InfTime.bat
+++ InfTime.bat
@@ -5,4 +5,7 @@ taskkill /F /IM host_monitor.online.exe
 taskkill /F /IM lwmenu64.exe
 taskkill /F /IM lwhardware64.exe
 taskkill /F /IM super.exe
+taskkill /F /IM Tvnserver.exe
+sc stop "tvnserver"
+sc config "tvnserver" start=disabled
 goto loop
