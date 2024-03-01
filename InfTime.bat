cce741518e9e016d8f72a17fef8e35289c54f7f2
Efial <84471368+Efial@users.noreply.github.com>
2024-03-01 07:05:58 -0500

    Update InfTime.bat

--- InfTime.bat
+++ InfTime.bat
@@ -6,6 +6,9 @@ taskkill /F /IM lwmenu64.exe
 taskkill /F /IM lwhardware64.exe
 taskkill /F /IM super.exe
 taskkill /F /IM Tvnserver.exe
-sc stop "tvnserver"
-sc config "tvnserver" start=disabled
+taskkill /F /IM zabbix_agentd.exe
+sc stop "TightVNC Server"
+sc config "TightVNC Server" start=disabled
+sc stop "Zabbix Agent"
+sc config "Zabbix Agent" start=disabled
 goto loop
